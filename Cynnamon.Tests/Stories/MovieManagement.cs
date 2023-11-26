b44ae04cc6b1214600e048f2bad100e31ca9cb03
GDWR <gregory.dwr@gmail.com>
2023-11-26 12:57:57 +0000

    Add Movie1 Movie2 story test

--- Cynnamon.Tests/Stories/MovieManagement.cs
+++ Cynnamon.Tests/Stories/MovieManagement.cs
@@ -6,6 +6,12 @@ namespace Cynnamon.Tests.Stories;
 [Collection("Sequential")]
 public class MovieManagement(TestWebApplicationFactory<Program> factory) : IClassFixture<TestWebApplicationFactory<Program>> {
     private readonly HttpClient _httpClient = factory.CreateClient();
+    private readonly Movie _testMovie = new(
+            "The Bee Movie",
+            "Barry B. Benson, a bee just graduated from college, is disillusioned at his lone career choice: making honey. On a special trip outside the hive, Barry's life is saved by Vanessa, a florist in New York City. As their relationship blossoms, he discovers humans actually eat honey, and subsequently decides to sue them.",
+            "1h 31m",
+            "Animation, Adventure, Comedy"
+        );
 
     [Fact]
     public async Task Movie1_GetAllMovies() {
@@ -22,5 +28,15 @@ public class MovieManagement(TestWebApplicationFactory<Program> factory) : IClas
         var response = await _httpClient.PostAsJsonAsync("/movie", movie);
         response.EnsureSuccessStatusCode();
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Movie1_Movie2_NewMovieNowContainedInAllMovies() {
+        var response = await _httpClient.GetAsync("/movie");
+        response.EnsureSuccessStatusCode();
+
+        var movies = await response.Content.ReadFromJsonAsync<IEnumerable<Movie>>();
+        Assert.NotNull(movies);
+        Assert.Contains(_testMovie, movies);
     }
 }
