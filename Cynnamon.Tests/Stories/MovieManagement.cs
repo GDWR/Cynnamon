3277eae2c238f1daa45ca70bd13b32352a6abae6
GDWR <gregory.dwr@gmail.com>
2023-11-26 14:23:58 +0000

    Add Movie3 test using sequence orderer

--- Cynnamon.Tests/Stories/MovieManagement.cs
+++ Cynnamon.Tests/Stories/MovieManagement.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 
 namespace Cynnamon.Tests.Stories;
 
-[Collection("MovieManagement")]
+[TestCaseOrderer(SequenceTestCaseOrderer.TypeName, SequenceTestCaseOrderer.AssemblyName)]
 public class MovieManagement(TestWebApplicationFactory<Program> factory) : IClassFixture<TestWebApplicationFactory<Program>> {
     private readonly HttpClient _httpClient = factory.CreateClient();
     private readonly AddMovieRequest _testMovieRequest = new(
@@ -13,29 +13,49 @@ public class MovieManagement(TestWebApplicationFactory<Program> factory) : IClas
             "Animation, Adventure, Comedy"
         );
 
-    [Fact]
+    [Fact, TestIndex(0)]
     public async Task Movie1_GetAllMovies() {
         var response = await _httpClient.GetAsync("/movie");
-        response.EnsureSuccessStatusCode();
-
         var movies = await response.Content.ReadFromJsonAsync<IEnumerable<Movie>>();
-        Assert.NotNull(movies);
+        Assert.Multiple(() => {
+            Assert.NotNull(movies);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        });
     }
 
-    [Fact]
+    [Fact, TestIndex(1)]
     public async Task Movie2_AddNewMovie() {
         var response = await _httpClient.PostAsJsonAsync("/movie", _testMovieRequest);
-        response.EnsureSuccessStatusCode();
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
 
-    [Fact]
+    [Fact, TestIndex(2)]
     public async Task Movie2_NewMovieNowContainedInAllMovies() {
         var response = await _httpClient.GetAsync("/movie");
-        response.EnsureSuccessStatusCode();
-
         var movies = await response.Content.ReadFromJsonAsync<IEnumerable<Movie>>();
-        Assert.NotNull(movies);
-        Assert.Contains(_testMovieRequest.Title, movies.Select(m => m.Title));
+        
+        Assert.Multiple(() => {
+            Assert.NotNull(movies);
+            Assert.Contains(_testMovieRequest.Title, movies.Select(m => m.Title));
+        });
+    }
+
+    [Fact, TestIndex(3)]
+    public async Task Movie3_UpdateExistingMovie()
+    {
+        // Making assumption we are using incrementing ids from 1, may change in the future.
+        var patchRequest = new PatchMovieRequest(null, "Updated description", null, null);
+        await _httpClient.PatchAsJsonAsync("/movie/1", patchRequest);
+
+        var movieRequest = await _httpClient.GetAsync("/movie/1");
+        var movie = await movieRequest.Content.ReadFromJsonAsync<Movie>();
+
+        Assert.Multiple(() => {
+            Assert.NotNull(movie);
+            Assert.Equal(_testMovieRequest.Title, movie.Title);
+            Assert.Equal("Updated description", movie.Description);
+            Assert.Equal(_testMovieRequest.Duration, movie.Duration);
+            Assert.Equal(_testMovieRequest.Genre, movie.Genre);
+        });
     }
 }
