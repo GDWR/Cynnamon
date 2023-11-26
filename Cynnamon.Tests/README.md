9348f30eb583b4a722470fa125f9075d3c92b7d4
GDWR <gregory.dwr@gmail.com>
2023-11-26 12:04:37 +0000

    Add test stories directory

---
+++ Cynnamon.Tests/README.md
@@ -0,0 +1,3 @@
+# Test structure
+
+Tests defined in `./Stories` aim to test a user story/requirement defined in the project [README.md](../README.md).
---
+++ Cynnamon.Tests/Stories/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace Cynnamon.Tests.Stories;
+
+public class Tests {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
